1de1e3848fbbcad5e5d2f2a6bcb06fd6d52276b8
CDiala <dialachibuzo@yahoo.com>
2022-08-06 19:37:50 +0100

    Refactored code, added code to store operation.

--- js/main.js
+++ js/main.js
@@ -9,42 +9,50 @@ let objCalculator = {
 };
 
 objCalculator.buttons.forEach((button) => {
+  let text = button.textContent;
   button.addEventListener("click", (e) => {
-    console.log(e);
+    // console.log(e);
     if (!button.dataset.action) {
-      if (button.textContent === "0") {
+      if (text === "0") {
         if (
           (!objCalculator.sign && objCalculator.num1 === "0") ||
           (objCalculator.sign && objCalculator.num2 === "0")
         ) {
         } else if (objCalculator.num1 === "0" && !objCalculator.sign) {
-          updateNumber(objCalculator, "num1", button.textContent);
+          updateNumber(objCalculator, "num1", text);
         } else if (objCalculator.num1 !== "0" && !objCalculator.sign) {
           let str = convertToString(objCalculator.num1);
-          str = appendString(str, button.textContent);
+          str = appendString(str, text);
           updateNumber(objCalculator, "num1", str);
         }
       } else if (objCalculator.num1 === "0" && !objCalculator.sign) {
-        updateNumber(objCalculator, "num1", button.textContent);
+        updateNumber(objCalculator, "num1", text);
       } else if (objCalculator.num1 !== "0" && !objCalculator.sign) {
         let str = convertToString(objCalculator.num1);
-        str = appendString(str, button.textContent);
+        str = appendString(str, text);
         updateNumber(objCalculator, "num1", str);
       }
-      console.log(objCalculator);
-    } else if (button.textContent === ".") {
+    } else if (text === ".") {
       if (!objCalculator.input.value.includes(".")) {
         let str = convertToString(
           !objCalculator.sign ? objCalculator.num1 : objCalculator.num2
         );
-        str = appendString(str, button.textContent);
+        str = appendString(str, text);
         updateNumber(objCalculator, !objCalculator.sign ? "num1" : "num2", str);
       }
       // displayNumber(objCalculator.input, objCalculator.num1);
-    } else if (button.textContent === "←") {
+    } else if (text === "←") {
       deleteNumber(objCalculator, "input");
+    } else if (text === "÷" || text === "×" || text === "−" || text === "+") {
+      if (objCalculator.num2 === "0") {
+        console.log("special char detected:", text);
+        updateNumber(objCalculator, "sign", text);
+      } else {
+        console.log("perform operation");
+      }
     }
     displayNumber(objCalculator.input, objCalculator.num1);
+    // console.log(objCalculator);
   });
 });
 
