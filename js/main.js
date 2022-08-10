8f5300d255aebca3aaf4118df5c7e0415ba7f7f7
CDiala <dialachibuzo@yahoo.com>
2022-08-10 23:28:14 +0100

    BODMAS implementation - phase 2

--- js/main.js
+++ js/main.js
@@ -5,16 +5,22 @@ let objCalculator = {
   error: document.querySelector(".error-display"),
   num1: "0",
   num2: "0",
+  tempNum: "",
   sign: "",
+  tempSign: "",
   lowSign: ["+", "−"],
   highSign: ["÷", "×"],
+  allSign: ["÷", "×", "+", "−"],
 };
 
 objCalculator.buttons.forEach((button) => {
   let text = button.textContent;
   button.addEventListener("click", (e) => {
     if (!button.dataset.action) {
-      if (text === "0") {
+      if (objCalculator.tempSign) {
+        let newTempNum = appendString(objCalculator.tempNum, text);
+        updateNumber(objCalculator, "tempNum", newTempNum);
+      } else if (text === "0") {
         if (
           (!objCalculator.sign && objCalculator.num1 === "0") ||
           (objCalculator.sign && objCalculator.num2 === "0")
@@ -54,7 +60,7 @@ objCalculator.buttons.forEach((button) => {
       }
     } else if (text === "←") {
       deleteNumber(objCalculator, "input");
-    } else if (text === "÷" || text === "×" || text === "−" || text === "+") {
+    } else if (objCalculator.allSign.includes(text)) {
       if (objCalculator.num2 === "0") {
         console.log("special char detected:", text);
         updateNumber(objCalculator, "sign", text);
@@ -67,50 +73,46 @@ objCalculator.buttons.forEach((button) => {
           2. save the returned answer in objCalculator.num1
           3. display the returned answer in objCalculator.output
           4. replace objCalculator.sign with <text>
+          5. reset num2 to '0'
         */
         if (
           objCalculator.lowSign.includes(text) ||
           objCalculator.highSign.includes(objCalculator.sign)
         ) {
-          // execute operation
-          console.log("matching signs found:", objCalculator.sign, text);
           let answer = operate(
             objCalculator.sign,
             objCalculator.num1,
             objCalculator.num2
           );
-
-          // save the returned answer in objCalculator.num1
           updateNumber(objCalculator, "num1", answer);
-          // display the returned answer in objCalculator.output
           displayNumber(objCalculator.output, answer);
-          // replace objCalculator.sign with <text>
           updateNumber(objCalculator, "sign", text);
-          // clear num2
           updateNumber(objCalculator, "num2", "0");
           console.log(objCalculator);
-        }
-        /*
-        if (<text> is + or - && obj.sign is + or -)
-        OR
-        if (<text> is / or * && obj.sign is /, *, +, or -)
-          1. execute operation
-          2. save the returned answer in objCalculator.num1
-          3. display the returned answer in objCalculator.output
-          4. replace objCalculator.sign with <text>
+        } else if (
+          objCalculator.highSign.includes(text) &&
+          objCalculator.lowSign.includes(objCalculator.sign)
+        ) {
+          updateNumber(objCalculator, "tempSign", text);
+          console.log(objCalculator);
+          // sample: 2 + 3 * 4
+          // ELSE if (<text> is + or - && obj.sign is / or *): DONE
+          //   1. store new sign in a tempSign variable in objCalculator (DONE)
+          // updateNumber(objCalculator, "tempSign", text);
+          //   2. save the next number in a tempNum variable in objCalculator (to be created)
+          // updateNumber(objCalculator, "tempNum", text); DONE (line 21)
 
-        ELSE if (<text> is + or - && obj.sign is / or *):
-          1. store new sign in a tempSign variable in objCalculator (to be created)
-          2. save the next number in a tempNum variable in objCalculator (to be created)
-          3a. if sign is * or /, calculate num2 * tempNum OR num2 / tempNum and save the ans in num2. then save sign in tempSign and save next num in tempNum.
-          3b. if sign is + or - , 
-            i) calculate num1 +or- num2 *or/ tempNum and save answer in num1.
-            ii) update obj.sign to reflect the new sign from user.
-            iii) clear num2
-            iv) display answer in output
-            v) reset input to 0 (code already implemented - kindly confirm)
-          4. replace objCalculator.sign with <text>
-        */
+          // MODIFY THE PSEUDO CODE BELOW
+
+          //   3a. if next sign is * or /, calculate num2 * tempNum OR num2 / tempNum and save the ans in num2. then save sign in tempSign and save next num in tempNum.
+          //   3b. if sign is + or - ,
+          //     i) calculate num1 +or- num2 *or/ tempNum and save answer in num1.
+          //     ii) update obj.sign to reflect the new sign from user.
+          //     iii) clear num2
+          //     iv) display answer in output
+          //     v) reset input to 0 (code already implemented - kindly confirm)
+          //   4. replace objCalculator.sign with <text>
+        }
         console.log("perform operation");
       }
     } else if (button.textContent.includes("±")) {
